a7672673f24cad14b0e7dfd469de4fd9fc179c75
paul-goodall <paul.thomas.goodall@gmail.com>
2022-03-31 20:05:12 +1100

    cleaning garbage4

--- django_aliases.sh
+++ django_aliases.sh
@@ -270,6 +270,7 @@ EOF
 
   echo "\nINSTALLED_APPS.append('crispy_forms')\n" >> ${django_name}/settings.py
 
+
   echo "do_app: ${do_app}"
   if [ do_app = 1 ]; then
       echo "Downloading: custom_user_auth_app"
